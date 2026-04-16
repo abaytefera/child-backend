2d7ba7c26c909db37fcf1ef84b27173b168d7f00
abay tefera <abaytefera29@gmail.com>
2026-04-16 09:43:00 +0300

    Update mainBackend.js
    eshi update

--- mainBackend.js
+++ mainBackend.js
@@ -51,7 +51,7 @@ Connectdb()
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
   }
 });
